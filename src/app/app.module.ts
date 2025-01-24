36afe21ef1c755f274c9d9873cca7d778cb70292
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-01-24 10:45:48 +0100

    add layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './components/layout/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/layout/toolbar/toolbar.component';
+import { MainContentComponent } from './components/layout/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
