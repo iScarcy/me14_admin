7d3601d2324d11c6a87b82d22720929cf450adf1
iScarcy <scarcella.giuseppe@gmail.com>
2025-02-03 17:31:06 +0100

    update grid

--- src/app/app.costant.ts
+++ src/app/app.costant.ts
@@ -1,2 +1,2 @@
-export const baseGalleryApiUrl:string = "https://localhost:7065/api/Gallery/";
+export const baseGalleryApiUrl:string = "http://www.agescimessina14.org/fw/api/Gallery/";
 export const baseGalleryPublicImageUrl: string = "http://www.agescimessina14.org/public/gallery/"
