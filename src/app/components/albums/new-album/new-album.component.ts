d2d4eb0284167c1220e242c05f97523803c087b8
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-02-04 16:49:52 +0100

    add file input

--- src/app/components/albums/new-album/new-album.component.ts
+++ src/app/components/albums/new-album/new-album.component.ts
@@ -13,4 +13,31 @@ export class NewAlbumComponent {
   FC_title = new FormControl('',[
     Validators.required
   ])
+
+  display: FormControl = new FormControl("", Validators.required);
+  file_store!: FileList;
+  file_list: Array<string> = [];
+  
+  handleFileInputChange(l: FileList ): void {
+    this.file_store = l;
+    if (l.length) {
+      const f = l[0];
+      const count = l.length > 1 ? `(+${l.length - 1} files)` : "";
+      this.display.patchValue(`${f.name}${count}`);
+    } else {
+      this.display.patchValue("");
+    }
+  }
+
+  handleSubmit(): void {
+    debugger;
+    var fd = new FormData();
+    this.file_list = [];
+    for (let i = 0; i < this.file_store.length; i++) {
+      fd.append("files", this.file_store[i], this.file_store[i].name);
+      this.file_list.push(this.file_store[i].name);
+    }
+
+    // do submit ajax
+  }
 }
