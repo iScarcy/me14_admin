ad678f7d2b23bc6280e19aea9061ec7f78232a92
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-03-20 14:23:34 +0100

    login title

--- src/app/app.costant.ts
+++ src/app/app.costant.ts
@@ -1,3 +1,5 @@
 export const baseGalleryApiUrl:string = "http://www.agescimessina14.org/fw/api/Gallery/";
 export const baseGalleryPublicImageUrl: string = "http://www.agescimessina14.org/public/gallery/"
 export const baseTempImageUrl: string = "http://www.agescimessina14.org/fw/upload"
+
+export const baseSecurityApiUrl:string = "http://www.agescimessina14.org/fw/api/Security/";
