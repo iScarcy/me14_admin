27fc1c40f9b73e5bb3753ed46633dac8ccdd0b11
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-03-22 14:54:42 +0100

    dispatch login request

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppStateModel } from 'src/app/shared/store/Global/App.state';
+import { ILoginRequest } from 'src/app/shared/store/Login/login.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,34 @@ import { Component, OnInit, signal } from '@angular/core';
 })
 export class LoginComponent  {
   
+   constructor(private _store: Store<AppStateModel>){}
+
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
+
+  FC_login = new FormControl('',[
+    Validators.required
+  ])
+
+  FC_password = new FormControl('',[
+    Validators.required
+  ])
+
+  ok(){
+    
+    if(this.FC_login.valid && this.FC_password.valid){
+
+        var req : ILoginRequest={
+          username: this.FC_login.value!,
+          password: this.FC_password.value!
+        }
+
+    }
+    
+  }
+
+
 }
