3728550ea19fd1ad6d0a1bcb9eb816d2ed6ba52a
iScarcy <scarcella.giuseppe@gmail.com>
2025-04-06 01:15:21 +0200

    fix rounting main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { AlbumsComponent } from './components/albums/albums.component';
 import { FotoComponent } from './components/albums/foto/foto.component';
+import { MainContentComponent } from './components/layout/main-content/main-content.component';
  
 const routes: Routes = [
+      {path:"", component: MainContentComponent},
       {path:"gallery", component: GalleryComponent},
       {path:"gallery/:branca", component: AlbumsComponent},
       {path:"gallery/photo/:album", component: FotoComponent}
