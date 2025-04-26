a6fbb8f76c59e649d5d852020ee9ac2d122e644c
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-04-26 03:21:29 +0200

    add crypto password

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,20 +3,23 @@ import { Injectable } from '@angular/core';
 import { from, map, Observable, of } from 'rxjs';
 import { baseUsersApiUrl } from '../app.costant';
 import { IUserLoginResponse } from './rest/IUserLoginResponse';
-import { ILogin } from '../models/ILogin';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  key:string =  "abcdefghijklmnop";
+
   constructor(private httpEvents: HttpClient) { }
 
 
   loginuser(username: string, password:string):Observable<IUserLoginResponse>{
-       
+      
+    
       var url: string = baseUsersApiUrl+"SignIn"; 
-      return this.httpEvents.post<IUserLoginResponse>(url, {email:username, password:password}).pipe(
+      return this.httpEvents.post<IUserLoginResponse>(url, {email:username, password: this.encrypt(password)}).pipe(
         
         map(resp => ({
                   name:resp.name,
@@ -32,5 +35,15 @@ export class LoginService {
   logoutuser(username: string):Observable<string>{
    
     return from("");
+  }
+
+  encrypt(password:string):string {
+    var key = CryptoJS.enc.Utf8.parse('intermerdaschifo');
+    var iv = CryptoJS.enc.Utf8.parse('8080808080808080');
+    
+    var encryptedpassword = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(password), key,
+    { keySize: 128 / 8, iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+
+    return encryptedpassword.toString();
   }
 }
