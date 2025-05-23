55d4e91a3d50ed59bf229a99313dac6353d9e512
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-05-23 16:49:57 +0200

    add getUtility method to utility service

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IUtility } from './rest/IUtility';
+import { baseUtilityApiUrl } from '../app.costant';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,21 @@ import { Injectable } from '@angular/core';
 export class UtilityService {
 
     constructor(private httpEvents: HttpClient) { }
+
+    getUtility():Observable<IUtility[]>{
+
+      var url = baseUtilityApiUrl;
+
+       return this.httpEvents.get<Array<IUtility>>(url).pipe(
+            map(utility => utility.map(item => ({
+                  id:item.id,
+                  name:item.name,
+                  fileFullPath:item.fileFullPath,
+                  typeID:item.typeID,
+                  type:item.type
+            })))
+          );
+
+
+    }
 }
